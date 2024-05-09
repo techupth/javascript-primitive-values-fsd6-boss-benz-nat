ddb851c7e1b5e5d6b1317fd9c6de44b02119e6cd
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-05-09 12:54:13 +0700

    finished ex 1-8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,6 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalCartPrice = 27000;
+let totalOneQuarterDiscountPrice = totalCartPrice * 0.25;
+let totalCheckoutPrice = totalCartPrice - totalOneQuarterDiscountPrice;
--- ex-7.js
+++ ex-7.js
@@ -5,3 +5,6 @@ let verb = "am";
 let object = "A Software Developer";
 
 let sentence;
+// sentence = "I am A Software Developer";
+
+sentence = subject + " " + verb + " " + object;
